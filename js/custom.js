2745e91a28bee40da130033f787b27de22dfd3dc
Alumno <alumno@S-2.local>
2018-07-30 18:44:48 -0500

    V0.1 Menu Cambia de color de negro a blanco

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,44 @@
 new WOW().init();
+
+/*
+$(window).scroll(function(){
+    var top = window.scrollTop();
+    var logoWhite = $('.logoWhite');
+    var logoBlack = $('.logoBlack');
+    
+  
+    if(top>=500){
+       
+        
+    }
+    
+    
+});
+
+
+
+ if ($('nav.navbar').hasClass('top-nav-collapse')) {
+        $('.logoWhite').fadeOut();
+        $('.logoBlack').fadeIn();
+    } else {
+        $('.logoWhite').fadeIn();
+        $('.logoBlack').fadeOut();
+}
+*/
+
+$(window).scroll(function(){
+   toogleClass(); 
+});
+
+
+function toogleClass(){
+    
+    if ($('nav').hasClass('top-nav-collapse')) {
+           $('.logoWhite').fadeOut();
+            $('.logoBlack').fadeIn(); 
+    } else{
+        //No tiene la clase 
+        $('.logoWhite').fadeIn();
+        $('.logoBlack').fadeOut();
+    }
+}
