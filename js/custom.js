f050b8de835e265ff3a789d023c95099e45f5d91
Dan <dan@studio-8.co>
2018-08-02 18:24:11 -0500

    V0.3 Carrusel de Servicios

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,5 @@
 new WOW().init();
 
-/*
-$(window).scroll(function(){
-    var top = window.scrollTop();
-    var logoWhite = $('.logoWhite');
-    var logoBlack = $('.logoBlack');
-    
-  
-    if(top>=500){
-       
-        
-    }
-    
-    
-});
-
-
-
- if ($('nav.navbar').hasClass('top-nav-collapse')) {
-        $('.logoWhite').fadeOut();
-        $('.logoBlack').fadeIn();
-    } else {
-        $('.logoWhite').fadeIn();
-        $('.logoBlack').fadeOut();
-}
-*/
 
 $(window).scroll(function(){
    toogleClass(); 
@@ -42,3 +17,44 @@ function toogleClass(){
         $('.logoBlack').fadeOut();
     }
 }
+
+
+//$(window).scroll(function(){
+//   var top = $(this).scrollTop();
+//   var menu = $('nav');    
+//    
+//    if(top >= 200){
+//        menu.addClass('bg-dark2');
+//       }else{
+//           menu.removeClass('bg-dark2');
+//       }
+//    
+//});
+
+
+  $(".owl-carousel").owlCarousel({
+    items:4,
+    slideBy:2,
+    loop:true,
+    margin:30,
+    autoplay:true,
+    autoplayTimeout:3500,
+    autoplayHoverPause:true,
+      responsive:{
+        0:{
+            items:2
+        },
+        576:{
+            items:3
+        },
+        768:{
+           items:4 
+        },  
+        992:{
+            items:4
+        },
+        1200:{
+            items:5
+        }  
+    }
+  });
